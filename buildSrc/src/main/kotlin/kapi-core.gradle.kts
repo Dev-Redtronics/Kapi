2f1333f2820fb09a75945c36d65fd2e156909e6e
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-04-09 14:42:34 +0200

    feat: add kapi-core Gradle configuration with Kotlin support and testing setup

---
+++ buildSrc/src/main/kotlin/kapi-core.gradle.kts
@@ -0,0 +1,58 @@
+import dev.redtronics.buildSrc.Project
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    org.jetbrains.kotlin.jvm
+    org.jetbrains.kotlin.plugin.serialization
+}
+
+group = "${Project.GROUP}.${Project.NAME.lowercase()}.${project.name.lowercase()}"
+version = 
+
+repositories {
+    mavenCentral()
+}
+
+public val targetJavaVersion: JavaVersion = JavaVersion.VERSION_1_8
+java {
+    targetCompatibility = targetJavaVersion
+    sourceCompatibility = targetJavaVersion
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(targetJavaVersion.majorVersion))
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+kotlin {
+    explicitApi()
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
+    jvmToolchain(targetJavaVersion.majorVersion.toInt())
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        reports.junitXml.required = true
+        systemProperty("gradle.build.dir", layout.buildDirectory.get().asFile.absolutePath)
+
+        filter {
+            isFailOnNoMatchingTests = false
+        }
+
+        testLogging {
+            showExceptions = true
+            showStandardStreams = true
+            events = setOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+}
