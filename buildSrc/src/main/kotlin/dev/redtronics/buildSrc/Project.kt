9f9b9eff48ea5cb4438d4887552946a50212a353
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-04-09 14:56:44 +0200

    feat: add INCEPTION_YEAR constant to Project object

--- buildSrc/src/main/kotlin/dev/redtronics/buildSrc/Project.kt
+++ buildSrc/src/main/kotlin/dev/redtronics/buildSrc/Project.kt
@@ -3,4 +3,5 @@ package dev.redtronics.buildSrc
 public object Project {
     public const val NAME: String = "Kapi"
     public const val GROUP: String = "dev.redtronics"
+    public const val INCEPTION_YEAR: Int = 2025
 }
