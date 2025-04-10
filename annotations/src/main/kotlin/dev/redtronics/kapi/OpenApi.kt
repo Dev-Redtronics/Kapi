4d30ae87e98c0ae97687d37b89c63d31848909b0
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-04-10 14:19:25 +0200

    feat: add OpenApi annotation to define API specifications and target

---
+++ annotations/src/main/kotlin/dev/redtronics/kapi/OpenApi.kt
@@ -0,0 +1,7 @@
+package dev.redtronics.kapi
+
+@Target(AnnotationTarget.CLASS)
+public annotation class OpenApi(
+    public val spec: String,
+    public val apiTarget: ApiTarget = ApiTarget.SERVER
+)
