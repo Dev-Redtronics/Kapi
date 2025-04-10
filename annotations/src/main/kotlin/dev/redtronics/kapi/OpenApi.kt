015103dd03102b1e2a160ae1e7ae1c962a23675b
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-04-10 14:37:37 +0200

    fix: add missing comma in OpenApi annotation parameters

--- annotations/src/main/kotlin/dev/redtronics/kapi/OpenApi.kt
+++ annotations/src/main/kotlin/dev/redtronics/kapi/OpenApi.kt
@@ -3,5 +3,5 @@ package dev.redtronics.kapi
 @Target(AnnotationTarget.CLASS)
 public annotation class OpenApi(
     public val spec: String,
-    public val apiTarget: ApiTarget = ApiTarget.SERVER
+    public val apiTarget: ApiTarget = ApiTarget.SERVER,
 )
